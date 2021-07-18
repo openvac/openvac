28db0b689fb0595a5c1f7f8758369482c6cf24d7
Chakrit Wichian <service@chakrit.net>
2021-07-18 20:31:54 +0700

    [api] Adds ecto & phoenix

--- api/mix.exs
+++ api/mix.exs
@@ -11,7 +11,6 @@ defmodule Openvac.MixProject do
     ]
   end
 
-  # Run "mix help compile.app" to learn about applications.
   def application do
     [
       extra_applications: [:logger],
@@ -19,11 +18,14 @@ defmodule Openvac.MixProject do
     ]
   end
 
-  # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
-      # {:dep_from_hexpm, "~> 0.3.0"},
-      # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"}
+      {:ecto_sql, "~> 3.6"},
+      {:postgrex, "~> 0.15"},
+      {:phoenix, "~> 1.5"},
+      {:jason, "~> 1.2"},
+      {:plug_cowboy, "~> 2.5"},
+      {:cowlib, "~> 2.10", override: true}
     ]
   end
 end

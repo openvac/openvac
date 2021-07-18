5202098faa586183ae5f8c1d7cb57e568e4245af
Eing <zeing@outlook.com>
2021-07-18 23:34:19 +0700

    [Web] update lint

--- web/postcss.config.js
+++ web/postcss.config.js
@@ -1,10 +1,4 @@
 'use strict'
 module.exports = {
-  plugins: [
-    'tailwindcss',
-    'postcss-nested',
-    'postcss-import',
-    'postcss-preset-env',
-    'autoprefixer',
-  ],
+  plugins: ['tailwindcss', 'postcss-nested', 'postcss-import', 'postcss-preset-env', 'autoprefixer'],
 }

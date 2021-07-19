89df52668f26363cd1b5238ad74dd08906aa0237
Siraset Jirapatchandej <sirasetjir@lightnet.io>
2021-07-20 01:02:01 +0700

    Create iex configuration file

---
+++ api/.iex.exs
@@ -0,0 +1,15 @@
+IEx.configure(
+  history_size: 50,
+  inspect: [
+    pretty: true,
+    limit: 100,
+    width: 80
+  ],
+  alive_prompt: "%prefix(%node)%counter>",
+  width: 80,
+  default_prompt: "%prefix(%counter)>"
+)
+
+# import Ecto.Query
+# alias OpenVac.<Any>
+
